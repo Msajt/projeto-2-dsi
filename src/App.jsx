f51dce82851566b0e5e3094e2aaa5adef52ac32f
Paulo César <paulocesarmedeiross@gmail.com>
2023-05-28 21:52:40 -0300

    refazendo projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
 //import { useState } from "react";
-//import reactLogo from "./assets/react.svg";
-//import viteLogo from "/vite.svg";
 import "./App.css";
 
-import Login from "./components/Login";
-
 function App() {
-    return (
-        <>
-            <Login />
-        </>
-    );
+    return <></>;
 }
 
 export default App;
